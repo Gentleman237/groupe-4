a3408cc91d6b67d6711a8907cff233c9123ef483
EricAllane23 <ericngomsu@icloud.com>
2025-05-15 11:46:54 +0100

    Ajout des fichiers test.h , EXO/EXO.depend

---
+++ EXO/test.h
@@ -0,0 +1,11 @@
+#ifndef TEST_H_INCLUDED
+#define TEST_H_INCLUDED
+
+int verif_entier();
+
+void programmeClasseEtudiant() ;
+int classer(const void *a, const void *b) ;
+int classerT(const void *a, const void *b) ;
+void programmeClasseEntiers() ;
+
+#endif // TEST_H_INCLUDED
